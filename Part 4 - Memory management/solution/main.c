d1ed73c794070fdf135e9ae76296df050bfb77c5
Cryxtalix <fgx8p9t@protonmail.com>
2024-05-21 02:09:21 +0800

    Update part 4

--- Part 4 - Memory management/solution/main.c
+++ Part 4 - Memory management/solution/main.c
@@ -1,99 +1,75 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "driver/gpio.h"
-#include "esp32s3_setup.h"
+#include "tdisplays3.h"
 
-// Use only core 1
-#if CONFIG_FREERTOS_UNICORE
-static const BaseType_t app_cpu = 0;
-#else
-static const BaseType_t app_cpu = 1;
-#endif
+char *serial_input;
+bool ready = 0;
 
-// Log tags
-static const char *tag_a = "tag_a";
-static const char *tag_b = "tag_b";
-
-// Task handles
-static TaskHandle_t handle_a = NULL;
-static TaskHandle_t handle_b = NULL;
-
-char *str_buf = NULL;
-bool notice = false;
-
-static void task_a(void *params)
+/**
+ * @brief Listen for input from serial monitor, store all chars upon receiving newline to heap. Notify task b.
+ * 
+ * @param params 
+ */
+void task_A(void *params)
 {
-        char input_buffer[2];
-        int str_buf_len = 10;
+        char str_buf[32] = {'\0'};
+        int char_buf;
 
-        while (1) {
-                input_buffer[0] = fgetc(stdin);
-                input_buffer[1] = '\0';
-
-                if(str_buf == NULL) {
-                        str_buf_len = 10;
-                        str_buf = calloc(str_buf_len, sizeof(char));
-                        if (str_buf == NULL) {
-                                ESP_LOGE(tag_a, "Out of memory");
-                                esp_restart();
+  	while(1) {
+                char_buf = fgetc(stdin);
+                if (char_buf != EOF) {
+                        str_buf[strlen(str_buf)] = (char)char_buf;
+                        if ((char)char_buf == '\n') {
+                                serial_input = malloc(32 * sizeof(char));
+                                strcpy(serial_input, str_buf);
+                                memset(str_buf, '\0', 32);
+                                ready = 1;
                         }
                 }
-                
-                if (strlen(str_buf) == str_buf_len - 1) {
-                        str_buf_len += 10;
-                        str_buf = realloc(str_buf, str_buf_len * sizeof(char));
-                        if(str_buf == NULL) {
-                                ESP_LOGE(tag_a, "Out of memory");
-                                esp_restart();
-                        }
-                }
-
-                strncat(str_buf, input_buffer, 2);
-
-                if(input_buffer[0] == '\n') {
-                        notice = true;
-                }
-                vTaskDelay(50 / portTICK_PERIOD_MS);
+                vTaskDelay(10 / portTICK_PERIOD_MS);
         }
 }
 
-static void task_b(void *params)
+/**
+ * @brief Waits for task A, prints message from heap, free heap.
+ * 
+ * @param params 
+ */
+void task_B(void *params)
 {
         while(1) {
-                if(notice == true) {
-                        printf("%s", str_buf);
-                        free(str_buf);
-                        str_buf = NULL;
-                        notice = false;
-                        ESP_LOGI(tag_b, "Heap remaining: %d", xPortGetFreeHeapSize());
+                if (ready == 1) {
+                        printf("%s", serial_input);
+                        free(serial_input);
+                        ready = 0;
                 }
-                vTaskDelay(50 / portTICK_PERIOD_MS);
+                vTaskDelay(10 / portTICK_PERIOD_MS);
         }
 }
 
 void app_main(void)
 {
-        esp32s3_serial_setup();
+        char *ourTaskName = pcTaskGetName(NULL);
+        ESP_LOGI(ourTaskName, "Hello, starting up!\n");
 
-        xTaskCreatePinnedToCore(                // Specific to esp32 freertos
-                task_a,                         // Function to be called
-                "Task A",                       // Name of Task
-                2000,                           // Stack size (bytes in esp32, words in freertos)
-                NULL,                           // Parameter to pass to function
-                configMAX_PRIORITIES - 1,       // Task priority (0 to configMAX_PRIORITIES - 1)
-                &handle_a,                      // Task handle 
-                app_cpu);                       // Run in core 1 only
+        gpio_reset_pin(PIN_LCD_BL);
+        gpio_set_direction(PIN_LCD_BL, GPIO_MODE_OUTPUT);
 
-        xTaskCreatePinnedToCore(                // Specific to esp32 freertos
-                task_b,                         // Function to be called
-                "Task B",                       // Name of Task
-                2500,                           // Stack size (bytes in esp32, words in freertos)
-                NULL,                           // Parameter to pass to function
-                configMAX_PRIORITIES,           // Task priority (0 to configMAX_PRIORITIES - 1)
-                &handle_b,                      // Task handle 
-                app_cpu);                       // Run in core 1 only
+        xTaskCreatePinnedToCore( // Specific to esp32 freertos
+                task_A,       // Function to be called
+                "listen for input from serial",    // Name of Task
+                10000,            // Stack size (bytes in esp32, words in freertos)
+                NULL,            // Parameter to pass to function
+                1,               // Task priority (0 to configMAX_PRIORITIES - 1)
+                NULL,            // Task handle 
+                app_cpu          // Run in core 1 only
+        );
+
+        xTaskCreatePinnedToCore( // Specific to esp32 freertos
+                task_B,       // Function to be called
+                "prints string",    // Name of Task
+                10000,            // Stack size (bytes in esp32, words in freertos)
+                NULL,            // Parameter to pass to function
+                2,               // Task priority (0 to configMAX_PRIORITIES - 1)
+                NULL,            // Task handle 
+                app_cpu          // Run in core 1 only
+        );
 }
--- notes.md
+++ notes.md
@@ -17,6 +17,62 @@ An RTOS might take up too much memory and processing resources of a microcontrol
 
 ESPIDF runs a modified version of FreeRTOS to support the ESP32's symmetric multiprocessing architecture that utilises the multiple cores. [Here is a list of changes](https://docs.espressif.com/projects/esp-idf/en/release-v4.3/esp32/api-guides/freertos-smp.html) made to the [IDF FreeRTOS](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/freertos_idf.html).
 
+## Code
+`vTaskDelay(500 / portTICK_PERIOD_MS)` 500ms non-blocking delay for FreeRTOS  
+
+    xTaskCreatePinnedToCore( // Creates task with affinity to specified CPU core
+    	myFunc, // Function to be called
+    	"Special function", // Name of task
+    	1024, // Stack size (bytes in ESPIDF, words in FreeRTOS)
+    	NULL, // Parameter to pass to function
+    	1, // Task priority (0 to configMAX_PRIORITIES-1)
+    	&func_handle, // Task handle
+    	app_cpu // CPU core (static const BaseType_t app_cpu = 1)
+    );
+
+`vTaskStartScheduler()` Only in vanilla FreeRTOS, to start the task  
+
+# Part 3 - Task Scheduling
+States where tasks can be in:
+
+* Ready: Can run at any time, but not yet given CPU time by the scheduler
+* Running: Chosen by the scheduler to run
+* Blocked: Blocking API called, such as waiting for delay, queue or semaphore
+* Suspended: Explicitly suspended with vTaskSuspend(). Can only be resumed by vTaskResume().
+
+For a task, all the current values in ram and CPU registers during it's execution such as current program instructions and working memory is known as the context. When the scheduler chooses a different task to run, the context will be saved, while the incoming task's context will be loaded so that it can carry on where it left off earlier. The process of saving and restoring task execution context is known as context switching. This enables multiple tasks to run on a single CPU core.
+
+Much of this context is stored in the task stack, and therefore every task created has to have a minimum amount of stack memory allocated.
+
+## Code
+`vTaskSuspend()` Enters the suspended state  
+`vTaskResume()` Enters the ready state  
+
+# Part 4 - Memory Management
+
+* **Static memory** is determined at compile time, and stores **global and static variables**. This is known as **static allocation** and the variables here exist for the duration of the program.
+
+* **Stack memory** stores functions and local variables, which also includes arguments and return addresses. Stack is LIFO system, variables are continually pushed to the stack when calling functions, and popped off the stack upon returning from a function. The stack typically grows towards the heap.
+
+* **Heap memory** is used for dynamic allocation, where the programmer explicitly reserves memory through functions like malloc. Allocated memory has to be freed after use, or the heap might grow indefinitely, known as a memory leak. As it grows towards the stack, it might even overwrite areas of the stack causing undefined behaviour.
+
+## ESP32
+In ESP32, the heap contains dynamically allocated memory, tasks and kernel objects. 
+
+A task created with xTaskCreate() is assigned a portion of memory from the heap. The portion is divided up into a task control block(TCB) and stack for the task. The TCB is a stuct which contains information about the task like task stack address and priority level. The stack's size can be changed from xTaskCreate().
+
+Kernel objects include queues and semaphores.
+
+During dynamic allocation, the largest contiguous block of heap memory available is usually allocated. With repeated allocation and freeing of this heap memory, it might get fragmented, resulting in it growing faster than expected. Thus, FreeRTOS contains several selectable heap allocation schemes to choose from.
+
+In newer versions of FreeRTOS, you can use xTaskCreateStatic() to allocate static memory for tasks and kernel objects in static memory. This is useful for mission critical applications where memory leaks cannot be allowed to occur.
+
+## Code
+`pvPortMalloc()` Thread safe version of malloc for FreeRTOS
+`vPortFree(ptr)` Thread safe version of free for FreeRTOS
+`uxTaskGetStackHighWaterMark(NULL)` Get remaining stack memory in words
+`xPortGetFreeHeapSize()` Get remaining heap memory in bytes
+
 # Serial communication on ESP32-S3
 
 Successful setup would allow using standard C functions like printf and fgets. 
